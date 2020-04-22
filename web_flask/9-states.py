252d0e23c3b0dfc091abbe0b44e31efc6d8391e5
rakiasomai <1069@holbertonschool.com>
2020-04-22 22:51:29 +0000

    My work

---
+++ web_flask/9-states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# script that starts a Flask web application
+from models import *
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/states')
+def states():
+    states = storage.all("State")
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>')
+def states_given_id(id):
+    states = storage.all("State")
+    found = ""
+    for s_id in states:
+        if s_id == id:
+            found = states[s_id]
+
+    return render_template('9-states.html',
+                           state=found_state)
+
+
+@app.teardown_appcontext
+def teardown(err):
+    storage.close()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
